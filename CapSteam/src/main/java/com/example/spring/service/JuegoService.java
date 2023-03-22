4bab9dd0b2cf535c780fecdbea1403cb8bbea338
Gomez <francisca.gomez-minet@capgemini.com>
2023-03-22 18:12:16 +0100

    metodos interfaz service

--- CapSteam/src/main/java/com/example/spring/service/JuegoService.java
+++ CapSteam/src/main/java/com/example/spring/service/JuegoService.java
@@ -1,5 +1,27 @@
 package com.example.spring.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.spring.model.Juego;
+
+
+
 public interface JuegoService {
+	
+		// LISTAR
+		public List<Juego>findAll();
+		
+		// RECUPERAR POR ID
+		public Optional<Juego>findById(int id);
+		
+		// MODIFICAR
+		public void saveJuego(Juego juego);
+		
+		// AÑADIR
+		public void addJuego(String name);
+		
+		// BORRAR
+		public void deleteJuego(int id);
 
 }
