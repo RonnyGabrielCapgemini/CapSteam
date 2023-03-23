ed146ad2ebea5f8e2f9f0fc025606f66188e472a
Leira Garcia <julian.leira-garcia@capgemini.com>
2023-03-23 10:13:47 +0100

    Creado servicio csv

--- CapSteam/src/main/java/com/example/spring/model/Juego.java
+++ CapSteam/src/main/java/com/example/spring/model/Juego.java
@@ -1,94 +1,133 @@
 package com.example.spring.model;
 
 
-import java.math.BigInteger;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="juegos")
 public class Juego {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="rank")
 	private int rank;
+	@Column(name="name")
 	private String name;
+	@Column(name="platform")
 	private String platform;
+	@Column(name="year")
 	private int year;
+	@Column(name="genre")
 	private String genre;
+	@Column(name="publisher")
 	private String publisher;
-	private BigInteger nasales;
-	private BigInteger eusales;
-	private BigInteger jpsales;
-	private BigInteger otherssales;
-	private BigInteger globalsales;
+	@Column(name="nasales")	
+	private Double na_sales;
+	@Column(name="eusales")
+	private Double eu_sales;
+	@Column(name="jpsales")
+	private Double jp_sales;
+	@Column(name="othersales")
+	private Double other_sales;
+	@Column(name="globalsales")
+	private Double global_sales;
 	
 	public Juego() {
 		super();
 	}
+	
 	public int getRank() {
 		return rank;
 	}
+
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPlatform() {
 		return platform;
 	}
+
 	public void setPlatform(String platform) {
 		this.platform = platform;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public String getGenre() {
 		return genre;
 	}
+
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
+
 	public String getPublisher() {
 		return publisher;
 	}
+
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
-	public BigInteger getNasales() {
-		return nasales;
+	public Double getNa_sales() {
+		return na_sales;
 	}
-	public void setNasales(BigInteger nasales) {
-		this.nasales = nasales;
+
+	public void setNa_sales(Double na_sales) {
+		this.na_sales = na_sales;
 	}
-	public BigInteger getEusales() {
-		return eusales;
+	public Double getEu_sales() {
+		return eu_sales;
 	}
-	public void setEusales(BigInteger eusales) {
-		this.eusales = eusales;
+
+	public void setEu_sales(Double eu_sales) {
+		this.eu_sales = eu_sales;
 	}
-	public BigInteger getJpsales() {
-		return jpsales;
+	public Double getJp_sales() {
+		return jp_sales;
 	}
-	public void setJpsales(BigInteger jpsales) {
-		this.jpsales = jpsales;
+
+	public void setJp_sales(Double jp_sales) {
+		this.jp_sales = jp_sales;
 	}
-	public BigInteger getOtherssales() {
-		return otherssales;
+	public Double getOther_sales() {
+		return other_sales;
 	}
-	public void setOtherssales(BigInteger otherssales) {
-		this.otherssales = otherssales;
+
+	public void setOther_sales(Double other_sales) {
+		this.other_sales = other_sales;
 	}
-	public BigInteger getGlobalsales() {
-		return globalsales;
+	public Double getGlobal_sales() {
+		return global_sales;
 	}
-	public void setGlobalsales(BigInteger globalsales) {
-		this.globalsales = globalsales;
+
+	public void setGlobal_sales(Double global_sales) {
+		this.global_sales = global_sales;
+	}
+	@Override
+	public String toString() {
+		return "Juego [rank=" + rank + ", name=" + name + ", platform=" + platform + ", year=" + year + ", genre="
+				+ genre + ", publisher=" + publisher + ", na_sales=" + na_sales + ", eu_sales=" + eu_sales
+				+ ", jp_sales=" + jp_sales + ", other_sales=" + other_sales + ", global_sales=" + global_sales + "]";
 	}
 	
 }
